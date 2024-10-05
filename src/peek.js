fb51d99f0e804f0d8eb56ee5e34a5eaced1b5d70
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-10-05 22:37:42 +0200

    refactor: remove not necessary headers

--- src/peek.js
+++ src/peek.js
@@ -1,17 +1,7 @@
 const collections = await fetch("https://learning.oreilly.com/api/v3/collections/", {
   "headers": {
-    "accept": "application/json",
-    "accept-language": "en-US,en;q=0.9,de;q=0.8,pl;q=0.7",
     "content-type": "application/json",
-    "sec-ch-ua": "\"Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
     "cookie": process.env.COOKIE,
-    "Referer": "https://learning.oreilly.com/playlists/",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
   },
   "body": null,
   "method": "GET"
