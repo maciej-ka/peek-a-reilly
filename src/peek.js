a4a831f87a1e139bb873496ec555e9950c206706
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-10-06 07:52:44 +0200

    feat: link to random chapter of random book

--- src/peek.js
+++ src/peek.js
@@ -1,8 +1,26 @@
-console.log("hello, v4")
+console.log("peek a reilly, v8")
 
-const run = () =>
-  fetch("https://learning.oreilly.com/api/v3/collections/")
-  .then(res => res.json())
-  .then(console.log)
+const rand = (array) => {
+  const i = Math.floor(Math.random() * array.length);
+  return array[i];
+}
 
-run()
+const peekSection = () => window.scrollTo(0, document.body.offsetHeight * Math.random())
+
+const peekChapter = async (path) => {
+  const res = await fetch(`https://learning.oreilly.com${path}`);
+  const json = await res.json();
+  const url = json.web_url;
+  const chapter = rand(json.chapters).split('/').pop()
+  console.log(url+chapter);
+}
+
+const peekBook = async () => {
+  const res = await fetch("https://learning.oreilly.com/api/v3/collections/");
+  const json = await res.json();
+  const parseBooks = (collection) => collection.content.map(b => b.api_url);
+  const paths = json.reduce((acc, c) => [...acc, ...parseBooks(c)], []);
+  peekChapter(rand(paths));
+}
+
+peekBook()
